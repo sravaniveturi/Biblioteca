d5826c6a75e02c1ff1a4a13ba3163e4c394e1e8b
Dharmendra Singh <dharmenn@thoughtworks.com>
2022-04-07 11:10:59 +0530

    Initial set-up

---
+++ src/test/java/com/tw/vapsi/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.vapsi.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
