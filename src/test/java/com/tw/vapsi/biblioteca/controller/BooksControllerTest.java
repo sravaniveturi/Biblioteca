fe58931db81687bba066f729d81b36eb1248c5b7
Dharmendra Singh <dharmenn@thoughtworks.com>
2022-04-07 12:29:43 +0530

    Add books controller with endpoint to list all books.

---
+++ src/test/java/com/tw/vapsi/biblioteca/controller/BooksControllerTest.java
@@ -0,0 +1,39 @@
+package com.tw.vapsi.biblioteca.controller;
+
+
+import com.tw.vapsi.biblioteca.model.Book;
+import com.tw.vapsi.biblioteca.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = BooksController.class)
+class BooksControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void shouldReturnListOfBooks() throws Exception {
+        List<Book> expectedBooks = Arrays.asList(Book.builder().name("Abc").build(), Book.builder().name("Xyz").build());
+        when(bookService.fetchAllBooks()).thenReturn(expectedBooks);
+
+        mockMvc.perform(get("/books"))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        verify(bookService, times(1)).fetchAllBooks();
+    }
+}
