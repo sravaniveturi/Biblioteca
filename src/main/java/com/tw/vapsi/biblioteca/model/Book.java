1c7168eada47d870b3fe56e4e4004ea3e9563617
Dharmendra Singh <dharmenn@thoughtworks.com>
2022-04-23 13:04:28 +0530

    Use generated identity as primary key for book.

--- src/main/java/com/tw/vapsi/biblioteca/model/Book.java
+++ src/main/java/com/tw/vapsi/biblioteca/model/Book.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
 public class Book {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
