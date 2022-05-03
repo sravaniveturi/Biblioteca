875d7fb959d9825e5dd02169a1eb67521e59e108
Dharmendra Singh <dharmenn@thoughtworks.com>
2022-05-03 16:55:24 +0530

    Remove Book entity, repository and service.

--- src/main/java/com/tw/vapsi/biblioteca/controller/BooksController.java
+++ src/main/java/com/tw/vapsi/biblioteca/controller/BooksController.java
@@ -1,20 +1,16 @@
 package com.tw.vapsi.biblioteca.controller;
 
-import com.tw.vapsi.biblioteca.model.Book;
-import com.tw.vapsi.biblioteca.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class BooksController {
-    @Autowired
-    private BookService bookService;
 
     @GetMapping("books")
-    public List<Book> books() {
-        return bookService.fetchAllBooks();
+    public List<String> books() {
+        return Arrays.asList("Book 1", "Book 2");
     }
 }
--- src/test/java/com/tw/vapsi/biblioteca/controller/BooksControllerTest.java
+++ src/test/java/com/tw/vapsi/biblioteca/controller/BooksControllerTest.java
@@ -2,20 +2,13 @@ package com.tw.vapsi.biblioteca.controller;
 
 
 import com.tw.vapsi.biblioteca.controller.helper.ControllerTestHelper;
-import com.tw.vapsi.biblioteca.model.Book;
-import com.tw.vapsi.biblioteca.service.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,21 +17,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BooksControllerTest extends ControllerTestHelper {
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private BookService bookService;
 
     @Test
     @WithMockUser
     void shouldReturnListOfBooks() throws Exception {
-        List<Book> expectedBooks = Arrays.asList(Book.builder().name("Abc").build(), Book.builder().name("Xyz").build());
-        when(bookService.fetchAllBooks()).thenReturn(expectedBooks);
-
 
         mockMvc.perform(get("/books"))
                 .andExpect(jsonPath("$.length()").value(2))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 
-        verify(bookService, times(1)).fetchAllBooks();
     }
 }
