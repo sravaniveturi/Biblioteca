7121df1be1492148aef07340e3af048799f6a6cb
vsravani <sravani.krishnan@gmail.com>
2022-06-28 11:04:23 +0530

    Add. book entity
    Co-authored-by: Nagu Kumar
    <nagupsgtech@gmail.com
    >

---
+++ src/main/java/com/tw/vapsi/biblioteca/model/Book.java
@@ -0,0 +1,31 @@
+package com.tw.vapsi.nullioteca.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String bookName;
+
+    private String authorName;
+
+    private Date dateOfPublication;
+
+    private int numberOfCopies;
+
+}
