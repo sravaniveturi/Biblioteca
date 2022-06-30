b99bea983466f69a5a4b4181578536c82fb81dd0
Arulmozhi Raju <arulmozhir2010@gmail.com>
2022-06-30 16:53:10 +0530

    Add welcometext as model attribute to home page

--- src/main/java/com/tw/vapsi/biblioteca/controller/HomeController.java
+++ src/main/java/com/tw/vapsi/biblioteca/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.tw.vapsi.biblioteca.controller;
 
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String base() {
-        return "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    public String getWelcome(Model model) {
+        String welcomeText = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        model.addAttribute("welcomeText", welcomeText);
+        return "index";
+
     }
+
 }
+
+
+
--- src/test/java/com/tw/vapsi/biblioteca/controller/HomeControllerTest.java
+++ src/test/java/com/tw/vapsi/biblioteca/controller/HomeControllerTest.java
@@ -1,14 +1,13 @@
 package com.tw.vapsi.biblioteca.controller;
 
-
 import com.tw.vapsi.biblioteca.controller.helper.ControllerTestHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = HomeController.class)
@@ -19,13 +18,8 @@ class HomeControllerTest extends ControllerTestHelper {
 
     @Test
     void shouldShowWelcomeMessage() throws Exception {
-        String expectedResponseContent = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
-        String actualResponseContent = mockMvc.perform(get("/").contentType("application/json"))
+        mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-
-        assertEquals(expectedResponseContent, actualResponseContent);
+                .andExpect(model().attribute("welcomeText", "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!"));
     }
 }
