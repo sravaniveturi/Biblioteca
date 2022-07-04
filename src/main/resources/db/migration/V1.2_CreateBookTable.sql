29589cd797c335130870c780a527a9f157415a60
Arulmozhi Raju <arulmozhir2010@gmail.com>
2022-07-04 06:43:04 +0530

    adding styles to index page

--- src/main/resources/db/migration/V1.2_CreateBookTable.sql
+++ src/main/resources/db/migration/V1.2_CreateBookTable.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS book{
-    id bigint,
-    bookName varchar(100) NOT NULL,
-    authorName varchar(100),
-    yearOfPublishing numeric NOT NULL,
-    numOfCopies numeric NOT NULL,
-    PRIMARY KEY (id)
-};
+CREATE TABLE IF NOT EXISTS book(
+        id bigint,
+        bookName varchar(100) NOT NULL,
+        authorName varchar(100),
+        yearOfPublishing numeric NOT NULL,
+        numOfCopies numeric NOT NULL,
+        PRIMARY KEY (id)
+);
+
