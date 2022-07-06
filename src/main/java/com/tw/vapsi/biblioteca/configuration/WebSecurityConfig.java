2c21b3f4b689b10c2f5d231fd4e94bef68d58b57
Radhika Anand <radhi4u.8@gmail.com>
2022-07-06 11:09:50 +0530

    Update. websecurity config
    Co-authored-by: Nagukumar <nagupsgtech@gmail.com>

--- src/main/java/com/tw/vapsi/biblioteca/configuration/WebSecurityConfig.java
+++ src/main/java/com/tw/vapsi/biblioteca/configuration/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/books","/home","/checkout","/books","/users/viewCheckout","/").authenticated()
+                .antMatchers("/books","/viewCheckout").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
